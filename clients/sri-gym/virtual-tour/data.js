ab507d6f52debad5227419dbca51ec1759efeb6a
testsinghvishal <karcsquare@gmail.com>
2021-05-20 19:32:58 -0700

    Updated a client project
    SRI GYM Ropar

---
+++ clients/sri-gym/virtual-tour/data.js
@@ -0,0 +1,485 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-reception-area",
+      "name": "Reception Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1175695384167295,
+          "pitch": 0.2202833728974607,
+          "rotation": 0,
+          "target": "6-zumba-area"
+        },
+        {
+          "yaw": -1.7795680233052895,
+          "pitch": 0.14473008689591538,
+          "rotation": 0,
+          "target": "8-cardio-area"
+        },
+        {
+          "yaw": -1.352600198328819,
+          "pitch": 0.2246933225601584,
+          "rotation": 0,
+          "target": "7-punching-bag-area"
+        },
+        {
+          "yaw": -0.006728117189487293,
+          "pitch": 0.269719445607997,
+          "rotation": 0,
+          "target": "1-center-gallary"
+        },
+        {
+          "yaw": -0.1540046432156963,
+          "pitch": 0.14529546989097852,
+          "rotation": 0,
+          "target": "2-corner-gallary"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-center-gallary",
+      "name": "Center Gallary",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.745002348368887,
+          "pitch": 0.29621857061120416,
+          "rotation": 0,
+          "target": "0-reception-area"
+        },
+        {
+          "yaw": -0.5089722843693192,
+          "pitch": 0.19686830252036458,
+          "rotation": 0,
+          "target": "7-punching-bag-area"
+        },
+        {
+          "yaw": 1.4451625060335616,
+          "pitch": 0.2625123595099783,
+          "rotation": 0,
+          "target": "2-corner-gallary"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-corner-gallary",
+      "name": "Corner Gallary",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.909094365821133,
+          "pitch": 0.31121757405258776,
+          "rotation": 0,
+          "target": "1-center-gallary"
+        },
+        {
+          "yaw": -1.9383614559977485,
+          "pitch": 0.11597363896536983,
+          "rotation": 0,
+          "target": "0-reception-area"
+        },
+        {
+          "yaw": -0.3324956558965919,
+          "pitch": 0.2014142274679962,
+          "rotation": 0,
+          "target": "5-heavy-workout-area"
+        },
+        {
+          "yaw": 0.05196242323876632,
+          "pitch": 0.19736962161940852,
+          "rotation": 0,
+          "target": "3-cross-cable-area"
+        },
+        {
+          "yaw": -0.06725927612231786,
+          "pitch": 0.09671338485985004,
+          "rotation": 0,
+          "target": "4-smith-machine-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cross-cable-area",
+      "name": "Cross Cable Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3313752905715521,
+          "pitch": 0.16731107544848633,
+          "rotation": 0,
+          "target": "2-corner-gallary"
+        },
+        {
+          "yaw": -0.8365812529746073,
+          "pitch": 0.11122171088680588,
+          "rotation": 0,
+          "target": "1-center-gallary"
+        },
+        {
+          "yaw": 0.046132328666750766,
+          "pitch": 0.41186889039217434,
+          "rotation": 0,
+          "target": "5-heavy-workout-area"
+        },
+        {
+          "yaw": 1.970699187437404,
+          "pitch": 0.19359179807609905,
+          "rotation": 0,
+          "target": "4-smith-machine-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-smith-machine-area",
+      "name": "Smith Machine Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12651820632222943,
+          "pitch": 0.28514657673190946,
+          "rotation": 0,
+          "target": "3-cross-cable-area"
+        },
+        {
+          "yaw": 0.12620739348746213,
+          "pitch": 0.07900629202081966,
+          "rotation": 0,
+          "target": "2-corner-gallary"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-heavy-workout-area",
+      "name": "Heavy Workout Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.47064666847881753,
+          "pitch": 0.174789931115944,
+          "rotation": 0,
+          "target": "2-corner-gallary"
+        },
+        {
+          "yaw": -1.2488845100431227,
+          "pitch": 0.20758869072671793,
+          "rotation": 0,
+          "target": "3-cross-cable-area"
+        },
+        {
+          "yaw": -1.7471809301348387,
+          "pitch": 0.09182037827623546,
+          "rotation": 0,
+          "target": "4-smith-machine-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-zumba-area",
+      "name": "Zumba Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.884162742940603,
+          "pitch": 0.30218184864626174,
+          "rotation": 0,
+          "target": "7-punching-bag-area"
+        },
+        {
+          "yaw": 1.5959838749337703,
+          "pitch": 0.2620161435537902,
+          "rotation": 0,
+          "target": "8-cardio-area"
+        },
+        {
+          "yaw": -1.8015199177758845,
+          "pitch": 0.15744321088271995,
+          "rotation": 0,
+          "target": "0-reception-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-punching-bag-area",
+      "name": "Punching Bag Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2975274621779986,
+          "pitch": 0.3582754226955984,
+          "rotation": 0,
+          "target": "6-zumba-area"
+        },
+        {
+          "yaw": -3.0161606249746846,
+          "pitch": 0.23492421081081005,
+          "rotation": 0,
+          "target": "8-cardio-area"
+        },
+        {
+          "yaw": 0.9254996538489113,
+          "pitch": 0.17879791567920478,
+          "rotation": 0,
+          "target": "0-reception-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-cardio-area",
+      "name": "Cardio Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.28800827063025025,
+          "pitch": 0.16392596411840543,
+          "rotation": 0,
+          "target": "7-punching-bag-area"
+        },
+        {
+          "yaw": 0.5487533143932541,
+          "pitch": 0.22931112156750544,
+          "rotation": 0,
+          "target": "6-zumba-area"
+        },
+        {
+          "yaw": 0.19079612817375313,
+          "pitch": 0.08765770253905103,
+          "rotation": 0,
+          "target": "0-reception-area"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SRI GYM",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
